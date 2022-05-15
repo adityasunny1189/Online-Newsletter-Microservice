fe61363bc1c482e40d0ddd6e33201dc9376f1eb0
Aditya Pathak <adityapathak@192.168.1.3>
2022-05-16 01:05:26 +0530

    subscription microservice complete all working fine

--- subscription_microservice/database/db.go
+++ subscription_microservice/database/db.go
@@ -12,11 +12,8 @@ const (
 	dsn = "root:Adisunny123@tcp(127.0.0.1:3306)/nuclei?charset=utf8mb4&parseTime=True&loc=Local"
 )
 
-var db *gorm.DB
-
 func InitializeDB() *gorm.DB {
-	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
